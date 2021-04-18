34b4777229bb03b5dc7f07b1e68b6029ac9552ac
master <eyeyeyeyeyeyeyeyey@gmail.com>
2021-04-18 09:19:58 +0800

    Initial commit

---
+++ w08-div/tku60.js
@@ -0,0 +1,56 @@
+function changeVideo(index) {
+  const video = document.querySelector('#video');
+  console.log('video', video);
+  switch (index) {
+    case 1:
+      video.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/RechrtUxfQc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+      break;
+    case 2:
+      video.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/MRWX49Furew" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+      break;
+    case 3:
+      video.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/METhdbL_iMw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+      break;
+    case 4:
+      video.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/ZyDbq-lEKTo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+      break;
+    case 5:
+      video.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/NlsrJbVvjaA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+      break;
+  }
+}
+function changeColor(index) {
+  const p = document.querySelector('#section');
+  switch (index) {
+    case 1:
+      p.style.backgroundColor = 'yellow';
+      break;
+    case 2:
+      p.style.backgroundColor = 'white';
+      break;
+    case 3:
+      p.style.backgroundColor = 'green';
+      break;
+  }
+  
+}
+function changePicture(index) {
+  const p = document.querySelector('#play')
+  switch(index) {
+      case 1:
+        video.innerHTML ='<img src="./image/img1.JPG">'
+      break;
+      case 2:
+        video.innerHTML ='<img src="./image/img2.JPG">'
+      break;
+      case 3:
+        video.innerHTML ='<img src="./image/img3.JPG">'
+      break;
+      case 4:
+        video.innerHTML ='<img src="./image/img4.JPG">'
+      break;
+      case 5:
+        video.innerHTML ='<img src="./image/img5.JPG">'
+      break;
+  }
+}
